9c70cc38c20579a5ba1a4a70af021eae6a7e4df3
tomname971-star <tomname971@gmail.com>
2026-07-08 19:53:13 +0200

    Order.java

--- order-pluginv2/src/main/java/org/order/order/Order.java
+++ order-pluginv2/src/main/java/org/order/order/Order.java
@@ -1,3 +1,4 @@
+cat > src/main/java/org/order/order/Order.java << 'EOF'
 package org.order.order;
 
 import org.bukkit.Bukkit;
@@ -6,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.order.order.db.Database;
@@ -234,3 +236,4 @@ public final class Order extends JavaPlugin implements Listener {
         p.sendMessage("§8§m----------------------------------------");
     }
 }
+EOF
